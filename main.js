d6ebe3172c0a796ba822f161843d9d4cf4888c1c
Wilton de Oliveira <104309027+Wilton21y@users.noreply.github.com>
2025-11-11 11:09:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('Hello World!');
+const bt = document.getElementById('bt')
+
+function op() {
+  alert('taca a mãe pra ver se cica')
+  pf.style.visibility='visible'
+}
+var pf = document.getElementById('photo');
+
+bt.addEventListener('click', op);
